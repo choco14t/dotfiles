7232b56498a1f6626ed8e0b18f7af523391e8d18
choco <8362391+choco14t@users.noreply.github.com>
2022-10-03 22:48:00 +0900

    add asdf setup
    and add condition for starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
-starship init fish | source
+if test -e ~/.asdf/asdf.fish
+    source ~/.asdf/asdf.fish
+end
+
+if type -q starship
+    starship init fish | source
+end
 
 source (dirname (status --current-filename))/alias.fish
