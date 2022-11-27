0851f737e43c64424989a50fd700dd9ca5b27f2d
yuma masuda <yuma.masuda@spacemarket.co.jp>
2022-11-27 18:13:16 +0900

    update colorscheme
    - alacritty
    - nvim

--- .config/nvim/lua/rc/config/nightfox.lua
+++ .config/nvim/lua/rc/config/nightfox.lua
@@ -7,7 +7,7 @@ nightfox.setup({
 	},
 })
 
-vim.cmd([[ colorscheme nordfox ]])
+vim.cmd([[ colorscheme nightfox ]])
 
 local compile_path = vim.fn.stdpath('cache') .. '/nightfox'
 if vim.fn.isdirectory(compile_path) == 0 then
