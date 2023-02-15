2a6317db252f111b94040abf3346f5ab9175479e
yuma masuda <yuma.masuda@spacemarket.co.jp>
2023-02-16 01:55:49 +0900

    add find buffer keymap

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,6 +2,7 @@ local ok, builtin = pcall(require, "telescope.builtin")
 if not ok then return end
 
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", ";b", builtin.buffers, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
