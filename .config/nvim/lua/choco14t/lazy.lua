5e8e02fcc1591c9018fc3637be5f4a7956854928
yuma masuda <yuma.masuda@spacemarket.co.jp>
2023-02-17 01:20:51 +0900

    add neovim plugins
    - lualine.nvim
    - trouble.nvim
    - vim-surround

--- .config/nvim/lua/choco14t/lazy.lua
+++ .config/nvim/lua/choco14t/lazy.lua
@@ -35,29 +35,41 @@ require("lazy").setup({
     },
   },
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v1.x",
     dependencies = {
       -- LSP Support
-      'neovim/nvim-lspconfig',             -- Required
-      'williamboman/mason.nvim',           -- Optional
-      'williamboman/mason-lspconfig.nvim', -- Optional
+      {"neovim/nvim-lspconfig"},             -- Required
+      {"williamboman/mason.nvim"},           -- Optional
+      {"williamboman/mason-lspconfig.nvim"}, -- Optional
 
       -- Autocompletion
-      'hrsh7th/nvim-cmp',         -- Required
-      'hrsh7th/cmp-nvim-lsp',     -- Required
-      'hrsh7th/cmp-buffer',       -- Optional
-      'hrsh7th/cmp-path',         -- Optional
-      'saadparwaiz1/cmp_luasnip', -- Optional
-      'hrsh7th/cmp-nvim-lua',     -- Optional
+      {"hrsh7th/nvim-cmp"},         -- Required
+      {"hrsh7th/cmp-nvim-lsp"},     -- Required
+      {"hrsh7th/cmp-buffer"},       -- Optional
+      {"hrsh7th/cmp-path"},         -- Optional
+      {"saadparwaiz1/cmp_luasnip"}, -- Optional
+      {"hrsh7th/cmp-nvim-lua"},     -- Optional
 
       -- Snippets
-      'L3MON4D3/LuaSnip',             -- Required
-      'rafamadriz/friendly-snippets', -- Optional
+      {"L3MON4D3/LuaSnip"},             -- Required
+      {"rafamadriz/friendly-snippets"}, -- Optional
     }
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {}
+    end
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
   },
   "folke/zen-mode.nvim",
   "mbbill/undotree",
+  "tpope/vim-surround",
   "nvim-tree/nvim-web-devicons",
 })
 
