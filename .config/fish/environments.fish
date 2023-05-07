d92769e9c3c8ac5fd91312802e4e0112c0973278
choco <8362391+choco14t@users.noreply.github.com>
2023-05-08 00:38:47 +0900

    fish: fix load env order

--- .config/fish/environments.fish
+++ .config/fish/environments.fish
@@ -1,11 +1,6 @@
-switch (uname)
-case Linux
-    if test -e ~/.asdf/asdf.fish
-      source ~/.asdf/asdf.fish
-    end
-case Darwin
-    if test -e $(brew --prefix asdf)/libexec/asdf.fish
-      source $(brew --prefix asdf)/libexec/asdf.fish
+if [ -d $FISH_DIR ] && [ -r $FISH_DIR ] && [ -x $FISH_DIR ]
+    for file in (ls $FISH_DIR/*.fish)
+        source $file
     end
 end
 
@@ -13,9 +8,13 @@ if type -q starship
     starship init fish | source
 end
 
-if [ -d $FISH_DIR ] && [ -r $FISH_DIR ] && [ -x $FISH_DIR ]
-    for file in (ls $FISH_DIR/*.fish)
-        source $file
+switch (uname)
+case Linux
+    if test -e ~/.asdf/asdf.fish
+      source ~/.asdf/asdf.fish
+    end
+case Darwin
+    if test -e (brew --prefix asdf)/libexec/asdf.fish
+      source (brew --prefix asdf)/libexec/asdf.fish
     end
 end
-
