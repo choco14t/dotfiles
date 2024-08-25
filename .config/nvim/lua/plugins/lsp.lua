c90e8ff6f4aed07ef4f4c295e8816ff3f803f4fc
choco <8362391+choco14t@users.noreply.github.com>
2024-08-25 18:49:19 +0900

    neovim: disable inlay hints

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,16 +1,24 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "eslint-lsp",
-      "graphql-language-service-cli",
-      "json-lsp",
-      "lua-language-server",
-      "prettier",
-      "rust-analyzer",
-      "stylua",
-      "terraform-ls",
-      "typescript-language-server",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint-lsp",
+        "graphql-language-service-cli",
+        "json-lsp",
+        "lua-language-server",
+        "prettier",
+        "rust-analyzer",
+        "stylua",
+        "terraform-ls",
+        "typescript-language-server",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
     },
   },
 }
