9f35588501cc16647d6d09b510ad17bc85aeaaac
choco <8362391+choco14t@users.noreply.github.com>
2024-10-04 23:29:45 +0900

    neovim: hide buffer close icon

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -107,4 +107,11 @@ return {
       },
     },
   },
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = { show_buffer_close_icons = false },
+    },
+  },
 }
