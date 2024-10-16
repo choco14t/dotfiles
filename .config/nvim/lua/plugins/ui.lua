66ccab7b901d1cd43568140118c99548e11e57ad
choco <8362391+choco14t@users.noreply.github.com>
2024-10-17 01:53:02 +0900

    neovim: modify separator style of lualine

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -88,6 +88,10 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = {
+      options = {
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+      },
       sections = {
         lualine_c = {
           {
