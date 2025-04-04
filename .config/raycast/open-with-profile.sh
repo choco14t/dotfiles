bb205e8a7ca93be501902eef1a97ab11c3157c47
choco <8362391+choco14t@users.noreply.github.com>
2025-04-04 09:34:36 +0900

    raycast: add custom script

---
+++ .config/raycast/open-with-profile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open with Profile
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🦊
+# @raycast.packageName Firefox
+# @raycast.argument1 { "type": "text", "placeholder": "Profile", "optional": true }
+
+# Documentation:
+# @raycast.description Open Firefox with input profile.
+# @raycast.author choco14t
+# @raycast.authorURL https://github.com/choco14t
+
+profile=$1
+
+# Using &>/dev/null to avoid print out error below
+# UNSUPPORTED (log once): POSSIBLE ISSUE: unit 1 GLD_TEXTURE_INDEX_2D is unloadable and bound to sampler type (Float) - using zero texture because texture unloadable
+/Applications/Firefox.app/Contents/MacOS/firefox -P $profile &>/dev/null
