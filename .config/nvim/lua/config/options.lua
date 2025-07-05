de9603efc67183724a3102233a3e9e1e967c9f2e
choco <8362391+choco14t@users.noreply.github.com>
2025-07-05 20:37:21 +0900

    nvim: use blink

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.snacks_animate = false
-vim.g.lazyvim_blink_main = false
+vim.g.lazyvim_blink_main = true
 
 vim.o.number = true
 vim.o.relativenumber = true
